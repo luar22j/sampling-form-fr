0f1ce0458905eaeb70245607a057ad91a56cf043
Luar <luarjs@gmail.com>
2024-11-12 13:30:59 +0100

    fix: add text on error page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 
 const NotFound = () => {
-  const { i18n } = useTranslation();
+  const { t, i18n } = useTranslation();
   const currentLang = i18n.language;
 
   return (
@@ -11,9 +11,21 @@ const NotFound = () => {
         to={`/${currentLang}`}
         className="bg-white py-4 items-center justify-center w-screen flex fixed z-4"
       >
-        {/* ... resto del código ... */}
+        <img src="/img/sampling-logo.png" className="w-[150px]" alt="logo" />
       </Link>
-      {/* ... resto del código ... */}
+      <div className="flex flex-col items-center justify-center h-screen">
+        <h1 className="title text-[#F04D79] text-4xl font-bold">
+          {t("common.notFound.title")}
+        </h1>
+        <p className="title text-lg">{t("common.notFound.description")}</p>
+
+        <a
+          href="https://sampling-innovations.com/"
+          className="active-button text-white bg-[#F04D79] px-4 py-2 rounded-md mt-5"
+        >
+          {t("common.notFound.cta")}
+        </a>
+      </div>
     </div>
   );
 };
